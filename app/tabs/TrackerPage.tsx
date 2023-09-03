6d9d73bceaaf1a360f815843560132d3bb2b441a
Flashminat0 <wyasaslowe@gmail.com>
2023-09-03 17:42:39 +0530

    loader for tracker

--- app/tabs/TrackerPage.tsx
+++ app/tabs/TrackerPage.tsx
@@ -8,11 +8,36 @@ import {ref as databaseRef, set, onValue, get, child} from 'firebase/database';
 import {jobData, UserData} from "./ScannerPage";
 import {Avatar, Button, Card, IconButton, MD3Colors} from "react-native-paper";
 import {clearLaptop, setLaptop} from "../../features/laptopSlice";
-import {useAppDispatch} from "../../features/redux";
+import {useAppDispatch, useAppSelector} from "../../features/redux";
 import {clearTracker} from "../../features/trackerSlice";
+import * as Progress from 'react-native-progress';
 
 const TrackerPage = () => {
     const dispatch = useAppDispatch();
+    const tracker = useAppSelector(state => state.tracker);
+
+    const failedImage = 'https://firebasestorage.googleapis.com/v0/b/research-cctv.appspot.com/o/failed.png?alt=media&token=90c50d27-cca4-4a83-891a-d78d5651d413'
+    const searchingImage = 'https://firebasestorage.googleapis.com/v0/b/research-cctv.appspot.com/o/searching.png?alt=media&token=e40a434d-76e9-4706-b1d1-b595f46e8b19'
+    const [cctvImage, setCctvImage] = useState<string>(searchingImage)
+
+    const [showLoading, setShowLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        if (tracker.found) {
+            setShowLoading(false)
+        }
+
+        if (tracker.tracking) {
+            setShowLoading(false)
+        }
+
+        if (tracker.problem) {
+            setShowLoading(false)
+            setCctvImage(failedImage)
+        }
+    }, [tracker]);
+
+
     const [userEmail, setUserEmail] = useState<string>('ERR@');
     const [currentJobTimestamp, setCurrentJobTimestamp] = useState<number>(0);
     const [currentJobStatus, setCurrentJobStatus] = useState<jobData | undefined>();
@@ -84,6 +109,7 @@ const TrackerPage = () => {
         })
     }
 
+
     return (
         <View>
             <View className={`flex flex-col justify-center items-center h-screen w-screen`}>
@@ -112,7 +138,18 @@ const TrackerPage = () => {
                         </Text>
                         <View className={`mx-5 my-1 mb-4`}>
                             <Card.Cover
-                                source={{uri: currentJobStatus?.image_of_laptop || 'https://picsum.photos/700'}}/>
+                                source={{uri: cctvImage}}/>
+                            {showLoading && <View className={`px-1 pt-1`}>
+                                <Progress.Bar
+                                    width={320}
+                                    useNativeDriver={true}
+                                    animationConfig={{bounciness: 1}}
+                                    className={' bg-gray-300 px-2'}
+                                    indeterminate={true}
+                                    animationType={'timing'}
+                                />
+                            </View>}
+
                         </View>
                     </View>
                     <View className={`px-3`}>
